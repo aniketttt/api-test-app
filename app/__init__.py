01d3b654f3d126d81cb5dcc06a1b06b964218e45
Aniket Sharma <54195664+aniketttt@users.noreply.github.com>
2024-03-15 10:32:06 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@db:5432/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@mydatabase:5432/mydatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
